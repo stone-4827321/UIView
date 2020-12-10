89776f53cf23ea4d8721330c95a4efebc6fbe2bd
Stone <wanglei@3k.com>
2020-12-10 14:07:18 +0800

    文档更新

---
+++ AutoLayout/AutoLayout/STView.h
@@ -0,0 +1,16 @@
+//
+//  STView.h
+//  AutoLayout
+//
+//  Created by stone on 2020/11/26.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface STView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AutoLayout/AutoLayout/STView.m
@@ -0,0 +1,20 @@
+//
+//  STView.m
+//  AutoLayout
+//
+//  Created by stone on 2020/11/26.
+//
+
+#import "STView.h"
+
+@implementation STView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
